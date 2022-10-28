a595c93a40ab9af35f3832ffbd0294770b9a492f
Ben Aubin <ben@benaubin.com>
2022-10-28 15:41:41 -0500

    flexible vector testing

--- melodies/tests/vectors.rs
+++ melodies/tests/vectors.rs
@@ -1,13 +1,13 @@
-use melodies::{patterns, DH25519, TAG_SIZE};
+use std::time::{Instant};
 
+use melodies::{patterns, BLAKE2s, DH25519, TAG_SIZE};
+
+use melodies_blake2::BLAKE2b;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
 #[serde(transparent)]
-pub struct Bytes(
-    #[serde(with = "hex")]
-    Vec<u8>
-);
+pub struct Bytes(#[serde(with = "hex")] Vec<u8>);
 
 #[derive(serde::Deserialize)]
 pub struct Vector {
@@ -17,12 +17,14 @@ pub struct Vector {
     pub init_ephemeral: Option<Bytes>,
     pub init_remote_ephemeral: Option<Bytes>,
     pub init_remote_static: Option<Bytes>,
+    pub init_psks: Option<Vec<Bytes>>,
     pub resp_prologue: Bytes,
     pub resp_static: Option<Bytes>,
     pub resp_ephemeral: Option<Bytes>,
     pub resp_remote_ephemeral: Option<Bytes>,
     pub resp_remote_static: Option<Bytes>,
-    pub handshake_hash: Bytes,
+    pub resp_psks: Option<Vec<Bytes>>,
+    pub handshake_hash: Option<Bytes>,
     pub messages: Vec<Message>,
 }
 
@@ -36,135 +38,194 @@ pub struct Message {
 
 #[derive(serde::Deserialize)]
 pub struct VectorFile {
-    vectors: Vec<Vector>
+    vectors: Vec<Vector>,
 }
 
 pub fn read_vectors(src: &'static str) -> VectorFile {
     serde_json::from_str(src).unwrap()
 }
 
-#[test]
-pub fn cacophony() {
-    let vectors = read_vectors(include_str!("./vectors/cacophony.json"));
-    for vector in vectors.vectors {
+fn do_handshake<
+    const DHLEN: usize,
+    const HASHLEN: usize,
+    K: melodies_core::crypto::DHKeypair<DHLEN>,
+    H: melodies_core::crypto::HashFunction<HASHLEN>,
+>(
+    mut initiator: melodies::HandshakeState<DHLEN, HASHLEN, K, H>,
+    mut responder: melodies::HandshakeState<DHLEN, HASHLEN, K, H>,
+    vector: &Vector,
+) {
+    if let Some(psks) = vector.init_psks.as_ref() {
+        if psks.len() > 0 {
+            assert_eq!(psks.len(), 1, "only one psk is currently supported");
+            initiator.insert_psk(&psks[0].0);
+        }
+    }
+
+    if let Some(psks) = vector.resp_psks.as_ref() {
+        if psks.len() > 0 {
+            assert_eq!(psks.len(), 1, "only one psk is currently supported");
+            responder.insert_psk(&psks[0].0);
+        }
+    }
+
+    let mut init_turn = true;
+    let mut buf = [0; 1024];
+    let mut iter = vector.messages.iter();
+    let (mut initiator, mut responder) = loop {
+        let message = iter.next().expect("handshake did not finish");
+        let payload_size = message.payload.len();
+        buf[..payload_size].copy_from_slice(&message.payload);
+        let message_size = if init_turn {
+            initiator.write_msg(&mut buf, payload_size)
+        } else {
+            responder.write_msg(&mut buf, payload_size)
+        };
+        let ciphertext = &mut buf[..message_size];
+        assert_eq!(&message.ciphertext, ciphertext);
+
+        let received = if init_turn {
+            responder.read_msg(ciphertext)
+        } else {
+            initiator.read_msg(ciphertext)
+        };
+
+        let received = received.expect("failed to read message");
+        assert_eq!(message.payload, received);
+
+        init_turn = !init_turn;
+
+        assert_eq!(initiator.is_finished(), responder.is_finished());
+        if initiator.is_finished() {
+            break (initiator.split(), responder.split());
+        }
+    };
+
+    if let Some(hh) = vector.handshake_hash.as_ref() {
+        assert_eq!(&initiator.hash[..], &hh.0[..]);
+        assert_eq!(&responder.hash[..], &hh.0[..]);
+    }
+
+    for message in iter {
+        let payload_size = message.payload.len();
+        buf[..payload_size].copy_from_slice(&message.payload);
+
+        let message_size = if init_turn {
+            initiator
+                .send
+                .encrypt(&[], &mut buf[..payload_size + TAG_SIZE])
+        } else {
+            responder
+                .send
+                .encrypt(&[], &mut buf[..payload_size + TAG_SIZE])
+        };
+        let ciphertext = &mut buf[..message_size];
+        assert_eq!(&message.ciphertext, ciphertext);
+
+        let received = if init_turn {
+            responder.recv.decrypt(&[], ciphertext)
+        } else {
+            initiator.recv.decrypt(&[], ciphertext)
+        };
+
+        let received = received.expect("failed to read message");
+        assert_eq!(message.payload, received);
+
+        init_turn = !init_turn;
+    }
+}
+
+pub fn test_vectors(vectors: VectorFile) {
+    let start = Instant::now();
+    let mut test_count = 0;
+    for vector in vectors.vectors.iter() {
         let mut parts = vector.protocol_name.split("_");
         parts.next();
         let pattern = parts.next().unwrap();
         let dh = parts.next().unwrap();
-        let cipher = parts.next().unwrap();
+        let cipher_name = parts.next().unwrap();
         let hash = parts.next().unwrap();
 
-        let pattern = match pattern {
-            "NN" => &patterns::NN,
-            "NK" => &patterns::NK,
-            "NX" => &patterns::NX,
-            "XN" => &patterns::XN,
-            "XK" => &patterns::XK,
-            "XX" => &patterns::XX,
-            "KN" => &patterns::KN,
-            "KK" => &patterns::KK,
-            "KX" => &patterns::KX,
-            "IN" => &patterns::IN,
-            "IK" => &patterns::IK,
-            "IX" => &patterns::IX,
-            _ => continue
+        let pattern = match patterns::ALL.iter().find(|p| p.name == pattern) {
+            Some(pat) => pat,
+            None => continue,
         };
 
-
-        let cipher = match cipher {
+        let cipher = match cipher_name {
             "ChaChaPoly" => &melodies_chacha20poly1305::ChaChaPoly,
             _ => continue,
         };
 
         match (dh, hash) {
             ("25519", "BLAKE2s") => {
-                let mut initiator = melodies::NoiseHandshake25519BLAKE2s::new(
+                let initiator = melodies::HandshakeState::<32, 32, _, BLAKE2s>::new(
                     cipher,
                     &pattern,
                     true,
                     &vector.init_prologue.0,
-                    vector.init_static.map(|b| DH25519::new(&b.0)).as_ref(),
-                    vector.init_ephemeral.map(|b| DH25519::new(&b.0)).as_ref(),
-                    vector.init_remote_static.map(|b| b.0.try_into().unwrap()),
-                    vector.init_remote_ephemeral.map(|b| b.0.try_into().unwrap()),
+                    vector
+                        .init_static
+                        .as_ref()
+                        .map(|b| DH25519::new(&b.0))
+                        .as_ref(),
+                    vector
+                        .init_ephemeral
+                        .as_ref()
+                        .map(|b| DH25519::new(&b.0))
+                        .as_ref(),
+                    vector
+                        .init_remote_static
+                        .as_ref()
+                        .map(|b| b.0.clone().try_into().unwrap()),
+                    vector
+                        .init_remote_ephemeral
+                        .as_ref()
+                        .map(|b| b.0.clone().try_into().unwrap()),
                 );
-                let mut responder = melodies::NoiseHandshake25519BLAKE2s::new(
+                let responder = melodies::HandshakeState::<32, 32, _, BLAKE2s>::new(
                     cipher,
                     pattern,
                     false,
                     &vector.resp_prologue.0,
-                    vector.resp_static.map(|b| DH25519::new(&b.0)).as_ref(),
-                    vector.resp_ephemeral.map(|b| DH25519::new(&b.0)).as_ref(),
-                    vector.resp_remote_static.map(|b| b.0.try_into().unwrap()),
-                    vector.resp_remote_ephemeral.map(|b| b.0.try_into().unwrap()),
+                    vector
+                        .resp_static
+                        .as_ref()
+                        .map(|b| DH25519::new(&b.0))
+                        .as_ref(),
+                    vector
+                        .resp_ephemeral
+                        .as_ref()
+                        .map(|b| DH25519::new(&b.0))
+                        .as_ref(),
+                    vector
+                        .resp_remote_static
+                        .as_ref()
+                        .map(|b| b.0.clone().try_into().unwrap()),
+                    vector
+                        .resp_remote_ephemeral
+                        .as_ref()
+                        .map(|b| b.0.clone().try_into().unwrap()),
                 );
-
-                let mut init_turn = true;
-                let mut buf = [0; 1024];
-                let mut iter = vector.messages.iter();
-                let transports = loop {
-                    let message = if let Some(message) = iter.next() {
-                        message
-                    } else {
-                        break None;
-                    };
-                    let payload_size = message.payload.len();
-                    buf[..payload_size].copy_from_slice(&message.payload);
-                    let message_size = if init_turn {
-                        initiator.write_msg(&mut buf, payload_size)
-                    } else {
-                        responder.write_msg(&mut buf, payload_size)
-                    };
-                    let ciphertext = &mut buf[..message_size];
-                    assert_eq!(&message.ciphertext, ciphertext);
-
-                    let received = if init_turn {
-                        responder.read_msg(ciphertext)
-                    } else {
-                        initiator.read_msg(ciphertext)
-                    };
-
-                    let received = received.expect("failed to read message");
-                    assert_eq!(message.payload, received);
-
-                    init_turn = !init_turn;
-
-                    assert_eq!(initiator.is_finished(), responder.is_finished());
-                    if initiator.is_finished() {
-                        break Some((initiator.split(), responder.split()));
-                    }
-                    
-                };
-                let (mut initiator, mut responder) = match transports {
-                    Some(t) => t,
-                    None => continue
-                };
-                for message in iter {
-                    let payload_size = message.payload.len();
-                    buf[..payload_size].copy_from_slice(&message.payload);
-
-                    
-                    let message_size = if init_turn {
-                        initiator.send.encrypt(&[], &mut buf[..payload_size + TAG_SIZE])
-                    } else {
-                        responder.send.encrypt(&[], &mut buf[..payload_size + TAG_SIZE])
-                    };
-                    let ciphertext = &mut buf[..message_size];
-                    assert_eq!(&message.ciphertext, ciphertext);
-
-                    let received = if init_turn {
-                        responder.recv.decrypt(&[], ciphertext)
-                    } else {
-                        initiator.recv.decrypt(&[], ciphertext)
-                    };
-
-                    let received = received.expect("failed to read message");
-                    assert_eq!(message.payload, received);
-
-                    init_turn = !init_turn;
-                }
+                do_handshake(initiator, responder, &vector)
+            },
             }
             _ => continue,
         };
+
+        test_count += 1;
     }
+    let dur = Instant::now().duration_since(start).as_millis();
+    println!("Tested {test_count} vectors in {dur}ms");
+}
+
+#[test]
+pub fn cacophony() {
+    let vectors = read_vectors(include_str!("./vectors/cacophony.json"));
+    test_vectors(vectors);
+}
+
+#[test]
+pub fn snow() {
+    let vectors = read_vectors(include_str!("./vectors/snow.json"));
+    test_vectors(vectors);
 }
